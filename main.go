e827ee8b04b3140d28dc8c115342efab5809cc3c
Akash R <akashrawatcats@gmail.com>
2024-11-14 22:32:53 +0530

    add addTask and Delete task function

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 )
 
-var fileDB os.File
-var TaskDB *[]Task
+var fileDB *os.File
+var TaskDB []Task
+var taskCounter int
 
 type Task struct {
 	TaskID     int       `json:"taskID"`
@@ -18,13 +20,13 @@ type Task struct {
 }
 
 func init() {
-
-	fileDB, err := os.OpenFile("tasks.json", os.O_CREATE|os.O_RDWR, 0644)
+	var err error
+	fileDB, err = os.OpenFile("tasks.json", os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
 		fmt.Println("Error: unable to open file", err)
 		os.Exit(1)
 	}
-
+	taskCounter = 1
 	fileStat, err := fileDB.Stat()
 	if err != nil {
 		fmt.Println("Error in reading file stats")
@@ -33,17 +35,27 @@ func init() {
 
 	fileSize := fileStat.Size()
 
+	if fileSize == 0 {
+		fileDB.Write([]byte("[]"))
+		return
+	}
+
 	fileBuffer := make([]byte, fileSize)
 
 	fileDB.Read(fileBuffer)
 
-	err = json.Unmarshal(fileBuffer, TaskDB)
+	err = json.Unmarshal(fileBuffer, &TaskDB)
 
+	for _, task := range TaskDB {
+		if task.TaskID >= taskCounter {
+			taskCounter = task.TaskID + 1
+		}
+	}
 	if err != nil {
 		fmt.Println("Error while unmarshalling", err)
 	}
 
-	fmt.Printf("%+v", TaskDB)
+	// fmt.Printf("Task is %+v", TaskDB)
 
 }
 
@@ -52,11 +64,99 @@ func main() {
 
 	argsWithoutFile := args[1:]
 
+	if len(argsWithoutFile) < 1 {
+		fmt.Println("Error: nothing passed")
+		os.Exit(1)
+	}
 	action := argsWithoutFile[0]
-	fmt.Println(action)
+
 	switch action {
 	case "add":
-
+		addTask(argsWithoutFile[1:])
+	case "delete":
+		deleteTask(argsWithoutFile[1:])
+	case ""
 	}
 
 }
+
+func addTask(newTask []string) {
+	if fileDB == nil {
+		fmt.Println("File pointer is nil")
+	} else {
+		fmt.Println("File opened successfully")
+	}
+
+	taskDescription := newTask[0]
+	timeNow := time.Now()
+
+	createTask := Task{
+		TaskID:     taskCounter,
+		TaskDesc:   taskDescription,
+		TaskStatus: "todo",
+		CreatedAt:  timeNow,
+	}
+	TaskDB = append(TaskDB, createTask)
+
+	truncateAndWrite(&TaskDB)
+
+}
+
+// func updateTask()     {}
+func deleteTask(inputs []string) {
+
+	taskIndex, err := strconv.Atoi(inputs[0])
+	if err != nil {
+		fmt.Println("Error: invalid type of index passed")
+		os.Exit(1)
+	}
+	var taskToDeleteIndex int
+	for i, r := range TaskDB {
+		if r.TaskID == taskIndex {
+			taskToDeleteIndex = i
+			break
+		}
+	}
+
+	TaskDB = append(TaskDB[:taskToDeleteIndex], TaskDB[taskToDeleteIndex+1:]...)
+	truncateAndWrite(&TaskDB)
+}
+
+func truncateAndWrite(writeData *[]Task) {
+
+	err := fileDB.Truncate(0)
+	if err != nil {
+		fmt.Println("Truncate error: ", err)
+	}
+	fileDB.Seek(0, 0)
+	fileData, err := json.Marshal(writeData)
+	if err != nil {
+		fmt.Println("erorr marshalling data")
+		os.Exit(1)
+	}
+
+	n, err := fileDB.WriteAt(fileData, 0)
+	if err != nil {
+		fmt.Println("Failed while writing to file, bytes written:", n, err)
+		fmt.Println(err)
+	}
+}
+
+func markDoneTask(inputs []string) {
+	taskIndex, err := strconv.Atoi(inputs[0])
+	if err != nil {
+		fmt.Println("Error: invalid type of index passed")
+		os.Exit(1)
+	}
+
+	for i, r := range TaskDB {
+		if r.TaskID == taskIndex {
+			r.TaskStatus = "done"
+		}
+	}
+
+	truncateAndWrite(&TaskDB)
+}
+
+// func markInProgTask() {}
+// func listTasks()      {}
